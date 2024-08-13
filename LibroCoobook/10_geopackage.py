1c99a64f79e5c011e335c746b7717c51880f46c1
HDRamosMendoza <heber.daniel.ramos.mendoza@gmail.com>
2024-08-13 10:59:46 -0500

    Se agrega comentarios

--- LibroCoobook/10_geopackage.py
+++ LibroCoobook/10_geopackage.py
@@ -19,24 +19,23 @@ from qgis.core import (
 from PyQt5.QtCore import QVariant
 
 """
- Se copia el SHP a un GEOPACKGE. Si no existe el geopacke te lo crea
+ Se copia el SHP a un Geopackage. Si no existe el Geopackage te lo crea
 """
 
 base_directory = Path("C:\Workspace_PyQt\PythonPyQT")
 path_temp      = base_directory.joinpath("LibroCoobook", "0_data","Acuifero.shp")
 path_geopackge = base_directory.joinpath("LibroCoobook", "0_package","my_new_file.gpkg")
 
-layer = QgsVectorLayer(str(path_temp), "shp_temp","ogr")
+layer = QgsVectorLayer(str(path_temp), "shp_temp","ogr") # VectorLayers
+#QgsProject.instance().addMapLayer(layer)
 save_options = QgsVectorFileWriter.SaveVectorOptions()
-save_options.layerName = "Acuifero_temp"
+save_options.layerName = "Acuifero_temp" # Nuevo nombre de SHP
 transform_context = QgsProject.instance().transformContext()
 
 fw = QgsVectorFileWriter.writeAsVectorFormatV3(
     layer,
-    str(path_geopackge),
+    str(path_geopackge), # Ruta de Geopackage 
     transform_context,    
     save_options
 )
-del fw 
-#lyr = QgsVectorLayer(r"D:\GIS\Терпланирование\sheet_folder\demo5.gpkg", 'my_layer', 'ogr')    
-#QgsProject.instance().addMapLayer(lyr)
+del fw
